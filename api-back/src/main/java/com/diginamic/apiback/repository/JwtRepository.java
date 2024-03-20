79e445980d33930d09a9b6b9284ae661af2ec810
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-03-20 12:56:10 +0100

    Ajouter la méthode findByToken à JwtRepository

--- api-back/src/main/java/com/diginamic/apiback/repository/JwtRepository.java
+++ api-back/src/main/java/com/diginamic/apiback/repository/JwtRepository.java
@@ -6,6 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.diginamic.apiback.models.Jwt;
 
+/**
+ * JwtRepository est une interface qui étend JpaRepository pour gérer les JWT en
+ * base de données.
+ * 
+ * @Optional findByToken: permet de récupérer un JWT à partir de son token
+ * 
+ * @param Jwt
+ */
 public interface JwtRepository extends JpaRepository<Jwt, Long> {
     Optional<Jwt> findByToken(String token);
 
