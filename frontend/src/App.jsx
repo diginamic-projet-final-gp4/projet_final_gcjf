ecdac3bc9ca1690f9ab6609e922368cdda501a9b
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-03-21 15:58:38 +0100

    Mise à jour de l'application avec les routes de connexion et de navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,53 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import UserContextProvider from './model/utils/context/UserContext'
-import Body from './components/Navigation/Body'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Login from "./pages/Login/Login";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    /*
+    Mise en commentaire de la récupération du token pour le moment car il n'est pas encore implémenté
+    */
+    // const token = localStorage.getItem("token");
+    // setIsConnected(!!token);
+
+    /* ----- DEV ----- 
+    Met le "isConnected" à true pour simuler un utilisateur connecté
+    */
+    setIsConnected(true);
+  }, []);
 
   return (
-    <>
-    {/* <UserContextProvider> */}
-      <Body />
-    {/* </UserContextProvider> */}
-    </>
-  )
+    <div className="App">
+      <Routes>
+        {/* Si on n'est pas connecté */}
+        {!isConnected && (
+          <>
+            <Route path="/" element={<Login />} />
+            {/* La route suivante permet de rediriger si on est pas connecter mais elle fait interférence même si on est connecté
+            Donc à voir pour plus tard
+
+            <Route path="*" element={<Navigate to="/login" />} />
+            */}
+          </>
+        )}
+
+        {/* Si on est connecté */}
+        {isConnected && (
+          <>
+            <Route path="/" element={<div>Home</div>} />
+            <Route path="/dashboard" element={<div>Dashboard</div>} />
+            <Route path="/profile" element={<div>Profile</div>} />
+            <Route path="*" element={<NotFound />} />
+          </>
+        )}
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
