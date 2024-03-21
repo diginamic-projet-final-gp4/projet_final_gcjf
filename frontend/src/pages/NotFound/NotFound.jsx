0a5d49b12fa59c3aadddde6f642979ca29370b2a
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-03-21 16:04:57 +0100

    Mise à jour de la page NotFound avec un message d'erreur et une animation

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -2,6 +2,16 @@ import "./NotFound.css";
 
 export default function NotFound() {
   return (
-    <div className="not-found">The page you are looking for does not exist</div>
+    <div className="not-found">
+      <h1>404</h1>
+      <p>La page demandée n&apos;existe pas ou a été déplacée</p>
+      <iframe
+        src="https://gifer.com/embed/7VE"
+        title="Not found"
+        width="480"
+        height="480"
+        allowFullScreen
+      ></iframe>
+    </div>
   );
 }
