49fa71bd006a9a25ee672da1c3e0c42eb07fbd8c
florian <furnari.florian@hotmail.fr>
2024-04-03 20:21:59 +0200

    commencement du tableau

--- frontend/src/pages/Rapport/ListCollabActuelTable.jsx
+++ frontend/src/pages/Rapport/ListCollabActuelTable.jsx
@@ -1,18 +1,62 @@
-import loadData from "../../model/utils/hooks.jsx"
-
+import loadData from "../../model/utils/hooks.jsx";
+import "./ListCollabActuelTable.css";
 /**
- * @param  {Object}  props  
- * @param  {number}  props.service 
+ * @param  {Object}  props
+ * @param  {number}  props.service
  * @param  {number}   props.month
  * @param  {number} props.year
  */
-export default function ListCollabActuelTable({service, month, year}) {
-  const {loadedData}=loadData(`http://localhost:8082/api/absence/service?id=${service}&month=${month}&year=${year}`)
 
-   let data = loadedData;
-   console.log(data)
-  return(
-  <>
-  </>
-  )
+function getDaysInMonth(year, month) {
+  const daysInMonth = new Date(year, month, 0).getDate();
+  const days = [];
+  for (let day = 1; day <= daysInMonth; day++) {
+    days.push(day);
+  }
+  return days;
+}
+export default function ListCollabActuelTable({ service, month, year }) {
+  const { loadedData } = loadData(
+    `http://localhost:8082/api/absence/service?id=${service}&month=${month}&year=${year}`
+  );
+
+  const daysOfMonth = getDaysInMonth(year, month);
+  let data = loadedData;
+  const fullNameSet = new Set()
+  const absenceDaysSet  = new Set()
+  
+  data.forEach((datas) => {
+    fullNameSet.add(datas.firstName + " " + datas.lastName) 
+    absenceDaysSet.add
+  });
+  console.log(data);
+  console.log(month)
+ 
+  return <>
+  <h2>liste des congés</h2>
+  <div id="table">
+    <div className="user" >
+      <h4>User</h4>
+      <div className="conteneurUser">
+      {Array.from(fullNameSet).map((fullName) => (
+              <p key={fullName}>{fullName}</p>
+            ))}
+          </div>
+        <div className="test">
+        {daysOfMonth.map(day => (
+          <p className="day" key={day}>{day}</p>
+          ))}
+        </div>
+    </div>
+    <div className="days">
+    {daysOfMonth.map(day => (
+          <p className="day" key={day}>{day}</p>
+        ))}
+        <div>
+
+        </div>
+    </div>
+
+  </div>
+  </>;
 }
