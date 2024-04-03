e0ab3c1c04409b356854f1dff566463bf203dab1
S-keyp <vicodin@orange.fr>
2024-04-03 20:52:42 +0200

    feat: help da bruh

--- frontend/src/pages/Rapport/ListCollabActuelTable.jsx
+++ frontend/src/pages/Rapport/ListCollabActuelTable.jsx
@@ -22,44 +22,98 @@ export default function ListCollabActuelTable({ service, month, year }) {
 
   const daysOfMonth = getDaysInMonth(year, month);
   let data = loadedData;
-  const fullNameSet = new Set()
-  const absenceDaysSet  = new Set()
-  
+  const fullNameSet = new Set();
+  const absenceDaysSet = new Set();
+
   data.forEach((datas) => {
-    fullNameSet.add(datas.firstName + " " + datas.lastName) 
-    absenceDaysSet.add
+    fullNameSet.add(datas.firstName + " " + datas.lastName);
+    absenceDaysSet.add;
   });
   console.log(data);
-  console.log(month)
- 
-  return <>
-  <h2>liste des congés</h2>
-  <div id="table">
-    <div className="user" >
-      <h4>User</h4>
-      <div className="conteneurUser">
-      {Array.from(fullNameSet).map((fullName) => (
+  console.log(month);
+  const maListe = [
+    {
+      fullName: "Jean mi",
+      absences: [
+
+      ]
+    },
+    {
+      fullName: "qdfglkjhqsdg mi",
+      absences: [
+
+      ]
+    },
+    {
+      fullName: "poiupoiu",
+      absences: [
+        
+      ]
+    },
+  ];
+
+  return (
+    <>
+      <h2>liste des congés</h2>
+      <div id="table">
+        <div className="user">
+          <h4>User</h4>
+
+
+
+
+
+          <table>
+            <thead>
+              <tr>
+                <th>User</th>
+                {daysOfMonth && daysOfMonth.map((val) => <th key={val}>{val}</th>)}
+              </tr>
+            </thead>
+            <tbody>
+              {
+                maListe && maListe.map((user, index) => {
+                  return <tr key={index}>
+                    <td>{user.fullName}</td>
+                    {user?.absences.map((absence) => {
+                      <td>{}</td>
+                    })}
+                  </tr>
+                })
+              }
+
+
+              <tr>
+                <td>Jean mi</td>
+                <td></td>
+                <td>5</td>
+                <td>48</td>
+                <td></td>
+                <td>654</td>
+                <td>67</td>
+                <td>67</td>
+              </tr>
+
+
+            </tbody>
+          </table>
+          
+          
+          {/* <div className="conteneurUser">
+            {Array.from(fullNameSet).map((fullName) => (
               <p key={fullName}>{fullName}</p>
             ))}
-          </div>
-        <div className="test">
-                {Array.from(fullNameSet).map((fullName) => (
-              <p key={fullName}>{fullName}</p>
-            ))}
-        {daysOfMonth.map(day => (
-          <p className="day" key={day}>{day}</p>
+          </div> */}
+        </div>
+        {/* <div className="days">
+          {daysOfMonth.map((day) => (
+            <p className="day" key={day}>
+              {day}
+            </p>
           ))}
-        </div>
-    </div>
-    <div className="days">
-    {daysOfMonth.map(day => (
-          <p className="day" key={day}>{day}</p>
-        ))}
-        <div>
-
-        </div>
-    </div>
-
-  </div>
-  </>;
+          <div></div>
+        </div> */}
+      </div>
+    </>
+  );
 }
